91a944b0c0e6d2cb2469cb53332baa7f9a74e18c
Ryan Ly <windask@hotmail.com>
2013-04-20 19:22:05 -0400

    Update Downloader

--- RyanLyAlienDownloader.py
+++ RyanLyAlienDownloader.py
@@ -1,11 +1,15 @@
 import urllib, re
 var = raw_input("Enter your URL to the subreddit: ")
-if var[0:3] == "www":
-    var = "".join(("http://",var))    
-elif var[0:6] == "reddit":
-    var = "".join(("http://www.",var))
-source = urllib.urlopen(var).read()
-m = source.find('id=\'header-img\'',0)
+if len(var) > 6:
+    if var[0:3] == "www":
+        var = "".join(("http://",var))    
+    elif var[0:6] == "reddit":
+        var = "".join(("http://www.",var))
+if (var[0:21] == "http://www.reddit.com"):
+    source = urllib.urlopen(var).read()
+    m = source.find('id=\'header-img\'',0)
+else:
+    m = -1
 if m == -1:
     print("Sorry!  Unable to find the image. Please check if link is valid or try again.")
 else:
